913d10ea963d7efb852c184e50d3e1f3989f2535
anderson.bosing <andersonbosing@MacBook-Air-de-Anderson.local>
2022-08-22 20:21:25 -0300

    Adicionada nova classe contabancaria

---
+++ ex-01-heranca/src/main/java/br/com/unipar/ex/heranca/model/ContaBancaria.java
@@ -0,0 +1,37 @@
+package br.com.unipar.ex.heranca.model;
+
+/**
+ *
+ * @author andersonbosing
+ */
+public class ContaBancaria {
+    
+    private String nomeCliente;
+    private Integer numeroConta;
+    private double saldo;
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public Integer getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(Integer numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+}
