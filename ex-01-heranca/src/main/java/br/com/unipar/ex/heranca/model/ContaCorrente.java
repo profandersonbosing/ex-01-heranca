508c4a148a7461bab59e117636651e8a5198b03d
anderson.bosing <andersonbosing@MacBook-Air-de-Anderson.local>
2022-08-26 20:07:07 -0300

    all

--- ex-01-heranca/src/main/java/br/com/unipar/ex/heranca/model/ContaCorrente.java
+++ ex-01-heranca/src/main/java/br/com/unipar/ex/heranca/model/ContaCorrente.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package br.com.unipar.ex.heranca.model;
 
 /**
  *
  * @author andersonbosing
  */
-public class ContaCorrente {
+public class ContaCorrente extends ContaBancaria {
+    
+    private double vlLimite;
+
+    public double getVlLimite() {
+        return vlLimite;
+    }
+
+    public void setVlLimite(double vlLimite) {
+        this.vlLimite = vlLimite;
+    }
+    
+    @Override
+    public void sacar(double valorSaque) {
+        
+        if (valorSaque > (super.getSaldo() + vlLimite)) {
+            System.out.println("Não foi possivel sacar pois o saldo será "
+                    + "negativo. Valor disponivel :" + super.getSaldo());
+        } else {
+            super.setSaldo(super.getSaldo() - valorSaque);
+            System.out.println("Valor Sacado. Novo Saldo: " + super.getSaldo());
+        }
+        
+    }
     
 }
